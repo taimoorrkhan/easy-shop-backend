31d3e33febc339493bfab3a694c44f041dc99f55
Taimoor Khan <82448357+taimooorr@users.noreply.github.com>
2023-09-30 10:40:36 +0500

    added products routes

--- models/productModel.js
+++ models/productModel.js
@@ -11,11 +11,21 @@ const productSchema = new mongoose.Schema({
   category: { type: mongoose.Schema.Types.ObjectId, ref: 'Category', required: true },
   countInStock: { type: Number, required: true, min: 0, max: 255 },
   rating: { type: Number, default: 0, max: 5, min: 1 },
+  numReviews: { type: Number, default: 0 },
   isFeatured: { type: Boolean, default: false },
   dateCreated: { type: Date, default: Date.now },
 
 
 });
+
+productSchema.virtual('id').get(function () {
+  return this._id.toHexString();
+});
+
+productSchema.set('toJSON', {
+  virtuals: true,
+});
  
 
 exports.Product = mongoose.model('Product', productSchema);
+exports.productSchema = productSchema;
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -3,14 +3,127 @@ const router = express.Router();
 
 const { Product } = require('../models/productModel');
 
+router.post(`/add`, async (req, res) => {
+  
+  const product = new Product({
+    name: req.body.name,
+    description: req.body.description,
+    richDescription: req.body.richDescription,
+    image: req.body.image,
+    images: req.body.images,
+    brand: req.body.brand,
+    price: req.body.price,
+    category: req.body.category,
+    countInStock: req.body.countInStock,
+    rating: req.body.rating,
+    numReviews: req.body.numReviews,
+    isFeatured: req.body.isFeatured,
+  })
+    
+
+  product = await product.save();
+
+  if (!product)
+    return res.status(404).send('the product cannot be created!')
+
+res.send(product);
+})
+
 router.get(`/`, async (req, res) => {
-  const productList = await Product.find();
+  let filter = {};
+  if (req.query.categories) { 
+    
+    filter = { category: req.query.categories.split(',') }
+  }
+  const productList = await Product.find(filter);
 
   if (!productList) {
     res.status(500).json({ success: false })
   }
   res.send(productList);
+}
+)
+
+router.get('/:id', async (req, res) => {
+  const product = await Product.findById(req.params.id).populate('category');
+
+  if (!product) {
+    res.status(500).json({ message: 'The product with the given ID was not found.' })
+  }
+  res.send(product);
 })
+
+//update a product
+
+router.put('/:id', async (req, res) => {
+  const product = await Product.findByIdAndUpdate(req.params.id,
+    {
+      name: req.body.name,
+      description: req.body.description,
+      richDescription: req.body.richDescription,
+      image: req.body.image,
+      images: req.body.images,
+      brand: req.body.brand,
+      price: req.body.price,
+      category: req.body.category,
+      countInStock: req.body.countInStock,
+      rating: req.body.rating,
+      numReviews: req.body.numReviews,
+      isFeatured: req.body.isFeatured,
+    },
+    { new: true }
+  )
+  if (!product)
+    return res.status(400).send('the product cannot be updated!')
+
+  res.send(product);
+}
+)
+
+//delete a product and validate the id
+
+router.delete('/:id', (req, res) => {
+  Product.findByIdAndRemove(req.params.id).then(product => {
+    if (product) {
+      return res.status(200).json({ success: true, message: 'the product is deleted!' })
+    } else {
+      return res.status(404).json({ success: false, message: 'product not found!' })
+    }
+  }).catch(err => {
+    return res.status(400).json({ success: false, error: err })
+  })
+})
+
+
+//get the total number of products
+
+router.get(`/get/count`, async (req, res) => {
+  const productCount = await Product.countDocuments((count) => count)
+
+  if (!productCount) {
+    res.status(500).json({ success: false })
+  }
+  res.send({
+    productCount: productCount
+  });
+})
+
+//get the featured products only with user intrest using AI
+
+router.get(`/get/featured/:count`, async (req, res) => {
+  const count = req.params.count ? req.params.count : 0
+  const products = await Product.find({ isFeatured: true }).limit(+count)
+
+  if (!products) {
+    res.status(500).json({ success: false })
+  }
+  res.send(products);
+})
+
+
+
+
+
 
 
 
