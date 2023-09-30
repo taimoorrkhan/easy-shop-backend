0c04c314940d6f5f76646cc9c2388c9e7af3f3a5
Taimoor Khan <82448357+taimooorr@users.noreply.github.com>
2023-09-30 10:40:22 +0500

    added userSchema

--- models/userModel.js
+++ models/userModel.js
@@ -1,12 +1,64 @@
 const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
-  name: String,
-  image: String,
-  countInStock: {
-    type: Number,
-    required: true
-  }
+  name: {
+    type: String,
+    required: true,
+  },
+  email:{
+    type: String,
+    required: true,
+  },
+  passwordHash:{
+    type: String,
+    required: true,
+  },
+  street :{
+    type: String,
+    default: ''
+  },
+  apartment :{
+    type: String,
+    default: ''
+  },
+  city :{
+    type: String,
+    default: ''
+  },
+  zip :{
+    type: String,
+    default: ''
+  },
+  country :{
+    type: String,
+    default: ''
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+  dateCreated: {
+    type: Date,
+    default: Date.now,
+  },
+  isSeller: {
+    type: Boolean,
+    default: false,
+  },
 })
+userSchema.virtual('id').get(function () {
+  return this._id.toHexString();
+});
+userSchema.set('toJSON', {
+  virtuals: true,
+});
+
+
+
 
 exports.User = mongoose.model('User', userSchema);
+exports.userSchema = userSchema;
